94545db7f2e76f071b1b906af3ff7777f2ae08b9
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-02-27 00:54:13 -0300

    Add files via upload

---
+++ Aula 4/src/Main.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Zumbi zumbi1 = new Zumbi();
+        Zumbi zumbi2 = new Zumbi();
+
+        zumbi1.nome = "Faria";
+        zumbi2.nome = "Farinha";
+
+        zumbi1.andar();
+        zumbi2.andar();
+
+
+
+        System.out.println("Qual o nome do zumbi escolhido? ");
+        String entrada = input.nextLine();
+
+        if(entrada.equals("Faria")){
+            System.out.println("Quanto de vida ele vai ter ? ");
+            double vida = input.nextDouble();
+            input.nextLine(); //limpar buffer
+            zumbi1.vida=vida;
+
+            zumbi1.mostrarVida();
+            zumbi2.vida=300;
+        }else{
+            System.out.println("Quanto de vida ele vai ter ? ");
+            double vida = input.nextDouble();
+            input.nextLine(); //limpar buffer
+            zumbi2.vida=vida;
+            zumbi1.vida = 300;
+
+        }
+
+        System.out.println("Qual o zumbi vai transferir hp ? ");
+        String alvo = input.nextLine();
+
+
+        if(alvo.equals("Faria")){
+            System.out.println("Quanto de vida ele vai transferir hp ? ");
+            double hpdoado = input.nextDouble();
+            zumbi1.transfereVida(zumbi2,hpdoado);
+            zumbi1.mostrarVida();
+
+        }else{
+            System.out.println("Quanto de vida ele vai transferir hp ? ");
+            double hpdoado = input.nextDouble();
+            zumbi2.transfereVida(zumbi1,hpdoado);
+        }
+
+        zumbi1.mostrarVida();
+        zumbi2.mostrarVida();
+
+
+
+
+    }
+}
---
+++ Aula 4/src/Zumbi.java
@@ -0,0 +1,21 @@
+public class Zumbi {
+    String nome;
+    double vida;
+
+    public void andar(){
+        System.out.println(nome + " está andando");
+    }
+
+    public void mostrarVida(){
+        System.out.println("Nome zumbi :" + nome);
+        System.out.println("Vida :" + vida);
+    }
+
+    public void transfereVida(Zumbi receptor,double quantia){
+        vida-=quantia;
+        receptor.vida += quantia;
+        System.out.println("Vida atual após a perca : " + vida);
+    }
+
+
+}
