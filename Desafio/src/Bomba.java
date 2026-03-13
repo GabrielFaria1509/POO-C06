f23f3ef8c5cece1d830fb3171cb93c963a6b0eba
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-03-13 01:13:01 -0300

    Update Bomba.java

--- Desafio/src/Bomba.java
+++ Desafio/src/Bomba.java
@@ -1,12 +1,15 @@
 public class Bomba {
     boolean ativo;
     int levelbomba;
+    int dano;
 
     void MostrarStatus(){
         if (ativo = true){
-            System.out.println("Bomba ativa,KABOOM");
+            System.out.println("Bomba ativa  colocada ativa");
+            System.out.println("Dano: " + dano);
+            System.out.println("Level : " + levelbomba);
         }else{
-            System.out.println("Bomba inativa");
+            System.out.println("Bomba inativa colcoada");
         }
     }
 }
