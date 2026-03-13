1f04479c884d60cdffcfd730ac962579e93d8544
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-03-13 01:14:39 -0300

    Refactor bomb initialization and gameplay logic

--- Desafio/src/Main.java
+++ Desafio/src/Main.java
@@ -6,32 +6,81 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int quantidade = 0;
         Bomba[][] matriz = new Bomba[6][6];
+        Player player =  new Player();
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
-                matriz[i][j] = new Bomba();  //aqui eu preenchi
+                matriz[i][j] = new Bomba();
                 //guardo objeto no lugar null
+            }
         }
-    }
 
-        for (int i = 0; i < 6; i++) {    //preencher
+        System.out.println("Iniciando preenchimento campo minado");
+
+        Random rand = new Random();  //gero o criador de número aleatório
+
+        for (int i = 0; i < 6; i++) {    //preencher e montando o campo minado
             for (int j = 0; j < 6; j++) {
-                Random rand = new Random();
-                int aleatorio = rand.nextInt(21);  //até 20
-                if (aleatorio > 10) {
+                int aleatorio = rand.nextInt(31);  //até 30
+                if (aleatorio >= 26) {
                     matriz[i][j].ativo = true;
                     matriz[i][j].levelbomba = 2;
+                    matriz[i][j].dano = 5;
+                    matriz[i][j].MostrarStatus();
+                    quantidade++;
 
-                } else if (aleatorio > 7 && aleatorio < 10) {
+                } else if (aleatorio >= 20  && aleatorio < 26) {
                     matriz[i][j].ativo = true;
                     matriz[i][j].levelbomba = 1;
-                } else if (aleatorio < 7) {
-                    matriz[i][j].ativo = false;
+                    matriz[i][j].dano = 2;
+                    matriz[i][j].MostrarStatus();
+                    quantidade++;
+                } else{
+                      matriz[i][j].ativo = false;
 
                 }
             }
         }
+
+        System.out.println("Insira nome do Player : ");  //início jogo
+        Scanner entrada = new Scanner(System.in);
+        String nome = entrada.nextLine();
+        player.nome = nome;
+
+        player.vida = 20;
+
+
+            while(player.vida > 0 && quantidade > 0) {
+                System.out.println("Escolha uma posição(Linha/Coluna entre 0  e 5 ):");
+                int linha = entrada.nextInt();
+                int coluna = entrada.nextInt();
+
+                if (matriz[linha][coluna].ativo == false) {
+                    System.out.println("Não há bomba");
+                } else {
+                    matriz[linha][coluna].ativo = true;
+                    System.out.println("Bomba encontrada !");
+                    player.vida -= matriz[linha][coluna].dano;
+                    matriz[linha][coluna].ativo = false; //desativar a bomba
+                    quantidade = quantidade - 1;
+                    player.mostrarInfo();
+                }
+
+
+            }
+
+        if(player.vida > 0){
+            System.out.println("Você ganhou ! ");
+        }
+
+        System.out.println("VIda zerada ! ");
     }
 }
+
+
+
+
+
 
 
