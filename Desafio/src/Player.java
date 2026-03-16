8994fa8bb68d5acb728b287a95b01b838661fc29
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-03-16 19:39:46 -0300

    Create Player class with attributes and method

---
+++ Desafio/src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    String nome;
+    int vida;
+
+    void mostrarInfo(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Vida: " + vida);
+    }
+}
