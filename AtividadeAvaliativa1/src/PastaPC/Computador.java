cb5c946d158ff49a20dadc34bba634fe7faaf3b3
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-03-26 15:00:21 -0300

    Construtor,atributos,setters,getters implementados

--- AtividadeAvaliativa1/src/PastaPC/Computador.java
+++ AtividadeAvaliativa1/src/PastaPC/Computador.java
@@ -1,4 +1,41 @@
 package PastaPC;
 
+import PastaMemoriaUSB.MemoriaUSB;
+import PastaOS.Sistemaoperacional;
+
 public class Computador {
+    private String marca;
+    private float preco;
+    private Sistemaoperacional sistemaoperacional; //composição==preciso de construtor
+    private MemoriaUSB memoriaUSB; //agregação não preciso de construtor
+
+    public Computador(Sistemaoperacional sistemaoperacional) {
+        this.sistemaoperacional = sistemaoperacional;
+    }
+
+    //Setters e Getters
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public Sistemaoperacional getSistemaoperacional() {
+        return sistemaoperacional;
+    }
+
+    public void setSistemaoperacional(Sistemaoperacional sistemaoperacional) {
+        this.sistemaoperacional = sistemaoperacional;
+    }
 }
